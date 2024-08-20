271db5d1586f68920d418694e60754495e291c9e
Girish kumar <Girishkumar7077@users.noreply.github.com>
2024-08-20 19:57:11 +0530

    javascript

---
+++ index.js
@@ -0,0 +1,85 @@
+// three types of variables : var, let and const
+
+// var name= 0;
+// name= 20;
+// console.log(name);
+
+// const name= 0;
+// name= 10;
+// console.log(name);
+
+// console.log(fun1())
+
+//  function fun1(){
+//     let a= 10;
+//     console.log(a);
+//     return a
+//  }
+//  fun1()
+
+// operators
+
+// Addition(+): Combines two numbers to get their sum.
+
+// let a=7;
+//     b= 3;
+//     c= (a+b);
+//     console.log(c)
+
+// Subtraction (-): Finds the difference between two numbers by subtracting one from the other.
+
+// let a=15;
+//     b=8;
+//     c=(a-b);
+//     console.log(c)
+
+// Multiplication(*): Multiplies two numbers to get their product.
+
+// let a=6;
+//     b=4;
+//     c=(a*b);
+//     console.log(c)
+
+// Division (/): Divides one number by another to get the quotient.
+
+// let a=20;
+//     b=4;
+//     quotient=(a/b)
+//     console.log(quotient);
+
+// Modulus(%): Returns the remainder of the division of one number by another.
+
+// let a=10;
+//     b=3;
+//     remainder=(a/b)
+//     console.log(remainder)
+
+// Increament (++): Increases a number by one.
+
+// let a=2;
+//     b=3;
+//     c=4;
+//     d=(a+b);
+//     e=(d*c);
+//     console.log(e)
+
+// let a=5;
+//     b=10;
+//     c=15;
+//     d=(
+//         (a+b+c)/3
+//     )
+//     console.log(d)
+
+// Decrement(--): Decreases a number by one.
+
+// let a=7;
+//     b=5;
+//     c=++a;
+//     d=c*b;
+//     console.log(d)
+
+let a=7;
+    b=5;
+    c=((--a)*b)
+    console.log(c)
